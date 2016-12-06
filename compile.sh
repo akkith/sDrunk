bcfbeb2552244d607eb1e75803649cb9625348d3
aaadmy <atuy.adamay@gmail.com>
2016-12-06 14:15:31 +0900

    pull

---
+++ compile.sh
@@ -0,0 +1,6 @@
+# g++ -c -Wall -std=c++11 -o sDrunk.o sDrunk.cpp
+# g++ -MM -Wall -std=c++11 sDrunk.cpp > sDrunk.d
+# -include sDrunk:.o=.d
+# g++ -std=c++11 -g -Wall sDrunk.o
+
+g++ -std=c++11 -Wall -arch x86_64 -o sDrunk sDrunk.cpp
